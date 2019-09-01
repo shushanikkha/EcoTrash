54591b623073bcc1f459712a990d827f940ca17a
Shushan Khachatryan <shushankhachatryan@Shushans-MacBook-Pro.local>
2019-09-01 15:18:39 +0400

    EcoTrash

---
+++ EcoTrash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoTrash
+//
+//  Created by Shushan Khachatryan on 9/1/19.
+//  Copyright © 2019 Shushan Khachatryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
